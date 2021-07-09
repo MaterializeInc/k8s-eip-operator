b6e78fc54072d3c07ee7bb07055432fca5c26120
Alex Hunt <alex.hunt@materialize.com>
2021-07-08 17:52:02 -0700

    working management of EIPs

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ use std::time::Duration;
 
 use futures_util::StreamExt;
 use k8s_openapi::api::core::v1::{Node, Pod};
-use kube::api::{Api, ListParams};
-use kube::{Client, Resource};
+use kube::api::{Api, ListParams, Patch, PatchParams};
+use kube::Client;
 use kube_runtime::controller::{Context, Controller, ReconcilerAction};
 use kube_runtime::finalizer::{finalizer, Event};
 use rusoto_core::region::Region;
 use rusoto_core::RusotoError;
 use rusoto_ec2::{
-    AssociateAddressError, AssociateAddressRequest, AssociateAddressResult, DescribeAddressesError,
-    DescribeAddressesRequest, DescribeAddressesResult, DescribeInstancesError,
-    DescribeInstancesRequest, DescribeInstancesResult, DisassociateAddressError,
-    DisassociateAddressRequest, Ec2, Ec2Client,
+    AllocateAddressError, AssociateAddressError, DescribeAddressesError, DescribeInstancesError,
+    DescribeInstancesRequest, DescribeInstancesResult, DisassociateAddressError, Ec2, Ec2Client,
+    ReleaseAddressError,
 };
 
-const EIP_LABEL: &'static str = "eip.aws.materialize.com/id";
+mod eip;
+
+const FIELD_MANAGER: &'static str = "eip.aws.materialize.com";
+const MANAGE_EIP_LABEL: &'static str = "eip.aws.materialize.com/manage";
 const FINALIZER_NAME: &'static str = "eip.aws.materialize.com/disassociate";
+const EXTERNAL_DNS_TARGET_ANNOTATION: &'static str = "external-dns.alpha.kubernetes.io/target";
 
 struct ContextData {
     namespace: String,
@@ -35,6 +38,25 @@ impl ContextData {
     }
 }
 
+async fn add_dns_target_annotation(
+    pod_api: &Api<Pod>,
+    pod_name: String,
+    eip_address: String,
+) -> Result<Pod, kube::Error> {
+    let patch = serde_json::json!({
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "annotations": {
+                EXTERNAL_DNS_TARGET_ANNOTATION: eip_address
+            }
+        }
+    });
+    let patch = Patch::Apply(&patch);
+    let params = PatchParams::apply(FIELD_MANAGER);
+    pod_api.patch(&pod_name, &params, &patch).await
+}
+
 async fn describe_instance(
     ec2_client: &Ec2Client,
     instance_id: String,
@@ -50,59 +72,48 @@ async fn describe_instance(
         .await
 }
 
-async fn associate_eip_with_pod_eni(
-    ec2_client: &Ec2Client,
-    eip_id: String,
-    eni_id: String,
-    pod_ip: String,
-) -> Result<AssociateAddressResult, RusotoError<AssociateAddressError>> {
-    ec2_client
-        .associate_address(AssociateAddressRequest {
-            allocation_id: Some(eip_id),
-            allow_reassociation: Some(true),
-            dry_run: None,
-            instance_id: None,
-            network_interface_id: Some(eni_id),
-            private_ip_address: Some(pod_ip),
-            public_ip: None,
-        })
-        .await
-}
-
-async fn describe_addresses(
-    ec2_client: &Ec2Client,
-    allocation_ids: Option<Vec<String>>,
-) -> Result<DescribeAddressesResult, RusotoError<DescribeAddressesError>> {
-    ec2_client
-        .describe_addresses(DescribeAddressesRequest {
-            allocation_ids,
-            dry_run: None,
-            filters: None,
-            public_ips: None,
-        })
-        .await
-}
-
-async fn disassociate_eip(
-    ec2_client: &Ec2Client,
-    association_id: String,
-) -> Result<(), RusotoError<DisassociateAddressError>> {
-    ec2_client
-        .disassociate_address(DisassociateAddressRequest {
-            association_id: Some(association_id),
-            dry_run: None,
-            public_ip: None,
-        })
-        .await
-}
-
 async fn apply(
     ec2_client: &Ec2Client,
-    node_api: Api<Node>,
-    eip_id: String,
+    node_api: &Api<Node>,
+    pod_api: &Api<Pod>,
     pod: Pod,
 ) -> Result<ReconcilerAction, Error> {
     println!("Associating...");
+    let pod_uid = pod
+        .metadata
+        .uid
+        .as_ref()
+        .ok_or_else(|| Error::MissingPodUid)?;
+    let addresses = &eip::describe_addresses(&ec2_client, pod_uid.to_owned())
+        .await?
+        .addresses
+        .ok_or_else(|| Error::MissingAddresses)?;
+    let (allocation_id, public_ip) = match addresses.len() {
+        0 => {
+            let response = eip::allocate_address(ec2_client, pod_uid.to_owned()).await?;
+            let allocation_id = response
+                .allocation_id
+                .ok_or_else(|| Error::MissingAllocationId)?;
+            let public_ip = response.public_ip.ok_or_else(|| Error::MissingPublicIp)?;
+            (allocation_id, public_ip)
+        }
+        1 => {
+            let allocation_id = addresses[0]
+                .allocation_id
+                .as_ref()
+                .ok_or_else(|| Error::MissingAllocationId)?;
+            let public_ip = addresses[0]
+                .public_ip
+                .as_ref()
+                .ok_or_else(|| Error::MissingPublicIp)?;
+            (allocation_id.to_owned(), public_ip.to_owned())
+        }
+        _ => {
+            return Err(Error::MultipleEipsTaggedForPod);
+            // TODO bail? Cleanup extras and return just one?
+        }
+    };
+
     let pod_ip = pod
         .status
         .as_ref()
@@ -150,34 +161,70 @@ async fn apply(
         .find_map(|nic| {
             nic.private_ip_addresses.as_ref()?.iter().find_map(|ip| {
                 match ip.private_ip_address.as_ref()? {
-                    x if x == pod_ip => Some(nic.network_interface_id.as_ref()?.to_owned()),
+                    x if x == pod_ip => {
+                        println!(
+                            "Found matching NIC: {} {} {}",
+                            nic.network_interface_id.as_ref().unwrap(),
+                            pod_ip,
+                            ip.private_ip_address.as_ref().unwrap()
+                        );
+                        Some(nic.network_interface_id.as_ref()?.to_owned())
+                    }
                     _ => None,
                 }
             })
         })
         .ok_or_else(|| Error::NoInterfaceWithThatIp)?;
 
-    associate_eip_with_pod_eni(&ec2_client, eip_id.to_owned(), eni_id, pod_ip.to_owned()).await?;
+    // TODO add if statement
+    // TODO If it is not associated, or associated with something else, associate it with the pod
+    // If it is associated with the pod, continue
+    eip::associate_eip_with_pod_eni(&ec2_client, allocation_id, eni_id, pod_ip.to_owned()).await?;
+    // TODO if pod doesn't have annotation for DNS, add it
+    let pod_name = pod
+        .metadata
+        .name
+        .as_ref()
+        .ok_or_else(|| Error::MissingPodName)?;
+    // TODO add if
+    add_dns_target_annotation(pod_api, pod_name.to_owned(), public_ip.to_owned()).await?;
     Ok(ReconcilerAction {
         requeue_after: Some(Duration::from_secs(300)),
     })
 }
 
-async fn cleanup(ec2_client: &Ec2Client, eip_id: String) -> Result<ReconcilerAction, Error> {
-    if let Some(association_id) = &describe_addresses(&ec2_client, Some(vec![eip_id.to_owned()]))
+async fn cleanup(ec2_client: &Ec2Client, pod: Pod) -> Result<ReconcilerAction, Error> {
+    let pod_uid = pod
+        .metadata
+        .uid
+        .as_ref()
+        .ok_or_else(|| Error::MissingPodUid)?;
+    //if pod
+    //    .metadata
+    //    .annotations
+    //    .contains_key(EXTERNAL_DNS_TARGET_ANNOTATION)
+    //{
+    //    // TODO remove DNS annotation
+    //    // Is this needed, or will external-dns clean up itself?
+    //}
+    let addresses = &eip::describe_addresses(&ec2_client, pod_uid.to_owned())
         .await?
         .addresses
-        .ok_or_else(|| Error::MissingAddresses)?[0]
-        .association_id
-    {
-        disassociate_eip(&ec2_client, association_id.to_owned()).await?;
+        .ok_or_else(|| Error::MissingAddresses)?;
+    for address in addresses {
+        if let Some(association_id) = &address.association_id {
+            eip::disassociate_eip(&ec2_client, association_id.to_owned()).await?;
+        }
+        if let Some(allocation_id) = &address.allocation_id {
+            // Is it actually possible the allocation_id won't exist?
+            eip::release_address(&ec2_client, allocation_id.to_owned()).await?;
+        }
     }
     Ok(ReconcilerAction {
         requeue_after: None,
     })
 }
 
-// https://dzone.com/articles/oxidizing-the-kubernetes-operator
 async fn reconcile(
     pod: Pod,
     context: Context<ContextData>,
@@ -186,12 +233,11 @@ async fn reconcile(
     let k8s_client = context.get_ref().k8s_client.clone();
     let pod_api: Api<Pod> = Api::namespaced(k8s_client.clone(), namespace);
     let node_api: Api<Node> = Api::all(k8s_client.clone());
-    let eip_id = pod.meta().labels[EIP_LABEL].to_owned();
     finalizer(&pod_api, FINALIZER_NAME, pod, |event| async {
         let ec2_client = context.get_ref().ec2_client.clone();
         match event {
-            Event::Apply(pod) => apply(&ec2_client, node_api, eip_id, pod).await,
-            Event::Cleanup(_pod) => cleanup(&ec2_client, eip_id).await,
+            Event::Apply(pod) => apply(&ec2_client, &node_api, &pod_api, pod).await,
+            Event::Cleanup(pod) => cleanup(&ec2_client, pod).await,
         }
     })
     .await
@@ -213,6 +259,10 @@ enum Error {
         #[from]
         source: kube::Error,
     },
+    #[error("Pod does not have a UID in it's metadata.")]
+    MissingPodUid,
+    #[error("Pod does not have a name in its metadata.")]
+    MissingPodName,
     #[error("Pod does not have an IP address.")]
     MissingPodIp,
     #[error("Pod does not have a node name in its spec.")]
@@ -221,6 +271,12 @@ enum Error {
     MissingProviderId,
     #[error("Node provider_id is not in expected format.")]
     MalformedProviderId,
+    #[error("Multiple elastic IPs are tagged with this pod's UID.")]
+    MultipleEipsTaggedForPod,
+    #[error("allocation_id was None.")]
+    MissingAllocationId,
+    #[error("public_ip was None.")]
+    MissingPublicIp,
     #[error("DescribeInstancesResult.reservations was None.")]
     MissingReservations,
     #[error("DescribeInstancesResult.reservations[0].instances was None.")]
@@ -231,6 +287,11 @@ enum Error {
     MissingAddresses,
     #[error("DescribeAddressesResult.addresses was None.")]
     NoInterfaceWithThatIp,
+    #[error("Rusoto allocate_address reported error: {source}")]
+    AllocateAddressError {
+        #[from]
+        source: rusoto_core::RusotoError<AllocateAddressError>,
+    },
     #[error("Rusoto describe_instances reported error: {source}")]
     DescribeInstancesError {
         #[from]
@@ -251,6 +312,11 @@ enum Error {
         #[from]
         source: rusoto_core::RusotoError<DisassociateAddressError>,
     },
+    #[error("Rusoto release_address reported error: {source}")]
+    ReleaseAddressError {
+        #[from]
+        source: rusoto_core::RusotoError<ReleaseAddressError>,
+    },
 }
 
 #[tokio::main]
@@ -262,7 +328,7 @@ async fn main() -> anyhow::Result<()> {
     let api = Api::<Pod>::namespaced(k8s_client.clone(), &namespace);
     let context: Context<ContextData> =
         Context::new(ContextData::new(namespace, k8s_client.clone(), ec2_client));
-    Controller::new(api, ListParams::default().labels(EIP_LABEL))
+    Controller::new(api, ListParams::default().labels(MANAGE_EIP_LABEL))
         .run(reconcile, on_error, context)
         .for_each(|reconciliation_result| async move {
             match reconciliation_result {
@@ -283,6 +349,7 @@ async fn main() -> anyhow::Result<()> {
 // https://rusoto.github.io/rusoto/rusoto_ec2/struct.DescribeInstancesRequest.html
 // aws ec2 describe-instances --profile mz-cloud-staging-admin --filters Name=network-interface.addresses.private-ip-address,Values=10.1.121.254
 
+// https://dzone.com/articles/oxidizing-the-kubernetes-operator
 // https://github.com/LogMeIn/k8s-aws-operator
 // https://github.com/kubernetes-sigs/external-dns/pull/2115/files
 //
@@ -299,3 +366,9 @@ async fn main() -> anyhow::Result<()> {
 // Create EIP with appropriate tags.
 // Assign label to pod with EIP assignment.
 // Create finalizer to destroy the EIP.
+//
+//
+//
+// SNAT must be enabled:
+// https://docs.aws.amazon.com/eks/latest/userguide/external-snat.html
+// kubectl set env daemonset -n kube-system aws-node AWS_VPC_K8S_CNI_EXTERNALSNAT=true
